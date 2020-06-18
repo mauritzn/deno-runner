50684009d376d3b80675efe8dafbc50c472606bf
Mauritz N <mauritz@europe.com>
2020-06-18 14:31:27 +0300

    Add demo projects

---
+++ demo_projects/denotrain/server.ts
@@ -0,0 +1,29 @@
+import {
+  Application,
+  Router,
+} from "https://deno.land/x/denotrain@v0.4.0/mod.ts";
+
+const app = new Application({ port: 3001 });
+const router = new Router();
+
+router.get("/", (ctx) => {
+  // Returning a string, JSON, Reader or Uint8Array automatically sets
+  // Content-Type header and no further router will match
+  return new Promise((resolve) => resolve("Hello world!"));
+});
+router.get("/greet/:name", async (ctx) => {
+  return `Hello ${ctx.req.params.name}!`;
+});
+
+router.get("/json", (ctx) => {
+  // Returning a json
+  return { "hello": "world" };
+});
+
+// Middleware
+app.use((ctx) => {
+  console.log(`${ctx.req.original.method} request for: ${ctx.req.path}`);
+});
+
+app.use("", router);
+await app.run();
